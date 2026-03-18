04177625623bd6d5943713f9e3e1a74dabf2e330
shadin <shadinmhd97@gmail.com>
2026-03-18 09:49:46 +0530

    feat: expo notification with test route

--- routers/user.route.ts
+++ routers/user.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getMe } from "../controllers/user.controller";
+import {
+  getMe,
+  updateFcmToken,
+  testNotification,
+} from "../controllers/user.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.get("/me", authMiddleware, getMe);
+router.post("/push-token", authMiddleware, updateFcmToken);
+router.post("/test-notification", authMiddleware, testNotification);
 
 export default router;
